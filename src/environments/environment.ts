e7174075616cb9cdf0c5e45e817c2a566a5abdcc
RamonFern <ramonfernandes9@gmail.com>
2021-07-17 16:15:25 -0300

    enviroment true teste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API: 'http://localhost:8080/api/v1/relatorio/'
   //API: 'https://relatorio-diario.herokuapp.com/api/v1/relatorio'
 };
